c878b658f0b47326f2dbe6f5086a30bff14b05a2
edward <son@binhsonnguyen.com>
2019-08-05 13:33:34 +0700

    Extract javascript sources to external file.

---
+++ introduction-to-programming/main.js
@@ -0,0 +1,5 @@
+function action() {
+    let inputValue = document.getElementById("input-value").value;
+    let message = "You inputted \"" + inputValue + "\"!";
+    document.getElementById("feedback").innerText = message;
+}
