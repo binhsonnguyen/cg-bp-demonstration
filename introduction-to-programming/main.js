778f930c1a2c62868478987725f99a4f6d6fb2c6
edward <son@binhsonnguyen.com>
2019-08-05 13:40:54 +0700

    Rename function that display inputted value.

--- introduction-to-programming/main.js
+++ introduction-to-programming/main.js
@@ -1,4 +1,4 @@
-function action() {
+function displayInputtedValue() {
     let inputValue = document.getElementById("input-value").value;
     let message = "You inputted \"" + inputValue + "\"!";
     document.getElementById("feedback").innerText = message;
