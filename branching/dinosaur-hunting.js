9e3d42d4f23950521a4d530c1363f648d8eb0918
edward <son@binhsonnguyen.com>
2019-08-07 11:21:33 +0700

    Extract VELOCITY constant.

--- branching/dinosaur-hunting.js
+++ branching/dinosaur-hunting.js
@@ -4,9 +4,10 @@ function registerKeyboardStrokeListener() {
 
 function onKeyDown() {
     console.log("A key has been stroke!");
+    const velocity = 5;
     let dinoElement = document.getElementById("dinosaurs");
     let dinoElementTopping = dinoElement.style.top;
     let top = parseInt(dinoElementTopping);
-    top += 5;
+    top += velocity;
     dinoElement.style.top = top + "px";
 }
