c0473dee838576c9288bbf261a9cea3d409ed1b1
edward <son@binhsonnguyen.com>
2019-08-07 11:29:10 +0700

    Extract keydown's code constant.

--- branching/dinosaur-hunting.js
+++ branching/dinosaur-hunting.js
@@ -4,10 +4,11 @@ function registerKeyboardStrokeListener() {
 
 function onKeyDown(event) {
     const velocity = 5;
+    const arrowDownKeyCode = 40;
     let dinoElement = document.getElementById("dinosaurs");
     let dinoElementTopping = dinoElement.style.top;
     let top = parseInt(dinoElementTopping);
-    if (event.which === 40) {
+    if (event.which === arrowDownKeyCode) {
         top += velocity;
     }
     dinoElement.style.top = top + "px";
