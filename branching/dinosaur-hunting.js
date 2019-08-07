22678f5c380bb9fee766837d9e18c3a9d26d57c5
edward <son@binhsonnguyen.com>
2019-08-07 11:36:24 +0700

    Controls dino left and right.

--- branching/dinosaur-hunting.js
+++ branching/dinosaur-hunting.js
@@ -4,18 +4,30 @@ function registerKeyboardStrokeListener() {
 
 function onKeyDown(event) {
     const velocity = 5;
+    const arrowLeftKeyCode = 37;
     const arrowUpKeyCode = 38;
+    const arrowRightKeyCode = 39;
     const arrowDownKeyCode = 40;
+
     let dinoElement = document.getElementById("dinosaurs");
+
     let dinoElementTopping = dinoElement.style.top;
     let top = parseInt(dinoElementTopping);
 
+    let dinoElementLeft = dinoElement.style.left;
+    let left = parseInt(dinoElementLeft);
+
     let key = event.which;
-    if (key === arrowUpKeyCode) {
+    if (key === arrowLeftKeyCode) {
+        left -= velocity;
+    } else if (key === arrowUpKeyCode) {
         top -= velocity;
+    } else if (key === arrowRightKeyCode) {
+        left += velocity;
     } else if (key === arrowDownKeyCode) {
         top += velocity;
     }
 
     dinoElement.style.top = top + "px";
+    dinoElement.style.left = left + "px";
 }
