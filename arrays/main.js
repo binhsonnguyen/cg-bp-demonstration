a0034e029315e4a0987a1977289676203dc3be0f
edward <son@binhsonnguyen.com>
2019-08-09 10:38:19 +0700

    Show all numbers of array in console.

--- arrays/main.js
+++ arrays/main.js
@@ -1,7 +1,7 @@
 function generate() {
     let numbers = [1, 2, 3, 4, 5];
-    console.log("first", numbers[0]);
 
-    let lastIndex = numbers.length - 1;
-    console.log("last", numbers[lastIndex]);
+    for (let i = 0; i < numbers.length; i++) {
+        console.log(numbers[i]);
+    }
 }
