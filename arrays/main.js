65ad43f29a69271b2cea4c36d7eb220d983c5af9
edward <son@binhsonnguyen.com>
2019-08-09 10:37:34 +0700

    Show first and last numbers of array in console.

--- arrays/main.js
+++ arrays/main.js
@@ -1,3 +1,7 @@
 function generate() {
     let numbers = [1, 2, 3, 4, 5];
+    console.log("first", numbers[0]);
+
+    let lastIndex = numbers.length - 1;
+    console.log("last", numbers[lastIndex]);
 }
