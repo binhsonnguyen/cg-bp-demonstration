15d668e78e0bd16eab9d3d336a3cd5a1f0308299
edward <son@binhsonnguyen.com>
2019-08-09 10:40:42 +0700

    Automation generate numbers instead of manual.

--- arrays/main.js
+++ arrays/main.js
@@ -1,5 +1,9 @@
 function generate() {
-    let numbers = [1, 2, 3, 4, 5];
+    let numbers = new Array(5);
+
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i] = i + 1;
+    }
 
     for (let i = 0; i < numbers.length; i++) {
         console.log(numbers[i]);
