91bf1c1591ed7c0f975244d5bf79ef6121732fa8
edward <son@binhsonnguyen.com>
2019-09-02 22:33:39 +0700

    Let developer use toolbox directly.

--- looping/main.js
+++ looping/main.js
@@ -1,6 +1,9 @@
-let pinCode;
-do {
-    pinCode = prompt("Input PIN Code to unlock!");
-} while (pinCode !== "1234");
+const devMode = true; // a configuration
+if (!devMode) {
+    let pinCode;
+    do {
+        pinCode = prompt("Input PIN Code to unlock!");
+    } while (pinCode !== "1234");
+}
 
 alert("Welcome");
