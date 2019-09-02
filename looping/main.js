d479e0be26ee3c32919891c7f80473c2ed8a99c4
edward <son@binhsonnguyen.com>
2019-09-02 22:23:08 +0700

    Put continue condition for while loop.

--- looping/main.js
+++ looping/main.js
@@ -1,7 +1,4 @@
-while (true) {
-    let pinCode = prompt("Input PIN Code to unlock!");
-    if (pinCode === "1234") {
-        alert("Welcome");
-        break;
-    }
+while (prompt("Input PIN Code to unlock!") !== "1234") {
 }
+
+alert("Welcome");
