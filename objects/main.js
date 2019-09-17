67afea15a2c04ebb96806e448469e91eb8d1df77
edward <son@binhsonnguyen.com>
2019-09-17 21:47:46 +0700

    Extract `renderReminder` function.

--- objects/main.js
+++ objects/main.js
@@ -1,12 +1,17 @@
 function display() {
     let remindersTable = document.getElementById("reminders-table");
     let reminderSubject = "Show main display";
+    let reminderElementView = renderReminder(reminderSubject);
+    remindersTable.innerHTML += reminderElementView;
+}
+
+function renderReminder(subject) {
     let reminderElementView =
         "<tr>" +
         "    <td>" +
         "        <input type='checkbox'>" +
         "    </td>" +
-        "    <td>" + reminderSubject + "</td>" +
+        "    <td>" + subject + "</td>" +
         "</tr>";
-    remindersTable.innerHTML += reminderElementView;
+    return reminderElementView;
 }
