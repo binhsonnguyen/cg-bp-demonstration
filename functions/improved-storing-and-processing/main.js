0a3f081148db6ed19ff66c3097b9925eb2ea90c6
edward <son@binhsonnguyen.com>
2019-09-17 07:12:02 +0700

    Extract `elements-utilities.js`.

--- functions/improved-storing-and-processing/main.js
+++ functions/improved-storing-and-processing/main.js
@@ -38,25 +38,3 @@ function parallelSwitchChange() {
     let switch2IsOn = isCheckBoxChecked('switch-b2');
     getElement("lamp-b").checked = switch1IsOn || switch2IsOn;
 }
-
-function getInputValue(id) {
-    let value = getElementAttribute(id, "value");
-    let isNumberInput = getElementAttribute(id, "type") === "number";
-    return isNumberInput ? +value : value;
-}
-
-function setInputValue(id, value) {
-    getElement(id).value = value;
-}
-
-function isCheckBoxChecked(id) {
-    return getElementAttribute(id, "checked");
-}
-
-function getElementAttribute(id, attr) {
-    return getElement(id)[attr]
-}
-
-function getElement(id) {
-    return document.getElementById(id);
-}
