dd493e3e74b1c23e609546dbe529a51cef250b3a
edward <son@binhsonnguyen.com>
2019-09-18 17:25:46 +0700

    Extract presenter's config constants.

--- classes/tetris-presenter.js
+++ classes/tetris-presenter.js
@@ -1,10 +1,13 @@
 function TerisPresenter(canvasId) {
     const _canvasId = canvasId;
+    const _width = 100;
+    const _height = 200;
+    const _backgroundColor = "darkslategray";
 
     this.displayBoard = () => {
         const gameBoardCanvas = document.getElementById(_canvasId);
-        gameBoardCanvas.width = 100;
-        gameBoardCanvas.height = 200;
-        gameBoardCanvas.style.backgroundColor = "darkslategray";
+        gameBoardCanvas.width = _width;
+        gameBoardCanvas.height = _height;
+        gameBoardCanvas.style.backgroundColor = _backgroundColor;
     };
 }
