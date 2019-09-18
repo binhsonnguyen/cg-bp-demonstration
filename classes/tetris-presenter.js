876d0531c4f68e3d79f04927c62aac80fe424f57
edward <son@binhsonnguyen.com>
2019-09-18 17:26:05 +0700

    Extract game-board's size to game object.

--- classes/tetris-presenter.js
+++ classes/tetris-presenter.js
@@ -1,13 +1,16 @@
 function TerisPresenter(canvasId) {
     const _canvasId = canvasId;
-    const _width = 100;
-    const _height = 200;
+    const _pixelPerCell = 10;
     const _backgroundColor = "darkslategray";
+    const _game = {
+        rows: 20,
+        cols: 10
+    };
 
     this.displayBoard = () => {
         const gameBoardCanvas = document.getElementById(_canvasId);
-        gameBoardCanvas.width = _width;
-        gameBoardCanvas.height = _height;
+        gameBoardCanvas.width = _game.cols * _pixelPerCell;
+        gameBoardCanvas.height = _game.rows * _pixelPerCell;
         gameBoardCanvas.style.backgroundColor = _backgroundColor;
     };
 }
