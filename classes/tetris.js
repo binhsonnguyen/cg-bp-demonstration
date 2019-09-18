98216c8942c90fb181d245b2e6a9f76bde9474d2
edward <son@binhsonnguyen.com>
2019-09-18 17:10:51 +0700

    Extract `TerisPresenter` class.

--- classes/tetris.js
+++ classes/tetris.js
@@ -1,9 +1,12 @@
-const presenter = {
-    canvasId: "game-board",
-    displayBoard: function () {
+const presenter = new TerisPresenter();
+
+function TerisPresenter() {
+    this.canvasId = "game-board";
+
+    this.displayBoard = function () {
         const gameBoardCanvas = document.getElementById(this.canvasId);
         gameBoardCanvas.width = 100;
         gameBoardCanvas.height = 200;
         gameBoardCanvas.style.backgroundColor = "darkslategray";
-    }
-};
+    };
+}
