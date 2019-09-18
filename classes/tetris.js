ee7c10a276f2da0c8895d0a57a6d3fced18a8dd0
edward <son@binhsonnguyen.com>
2019-09-18 17:08:39 +0700

    Extract canvas id property for presenter object.

--- classes/tetris.js
+++ classes/tetris.js
@@ -1,6 +1,7 @@
 const presenter = {
+    canvasId: "game-board",
     displayBoard: function () {
-        const gameBoardCanvas = document.getElementById("game-board");
+        const gameBoardCanvas = document.getElementById(this.canvasId);
         gameBoardCanvas.width = 100;
         gameBoardCanvas.height = 200;
         gameBoardCanvas.style.backgroundColor = "darkslategray";
