d207b39e039f3e7d35cfd037c3c7cf214c11d0f8
edward <son@binhsonnguyen.com>
2019-09-19 12:01:22 +0700

    Extract `_cell` object.

--- classes/tetris-presenter.js
+++ classes/tetris-presenter.js
@@ -5,6 +5,12 @@ function TerisPresenter(canvasId, game) {
     const _cellRadius = _pixelPerCell / 2;
     const _backgroundColor = "darkslategray";
 
+    const _cell = {
+        color: "red",
+        x: 2,
+        y: 4,
+    };
+
     this.displayBoard = () => {
         const gameBoardCanvas = document.getElementById(_canvasId);
         gameBoardCanvas.width = _game.getCols() * _pixelPerCell;
@@ -16,8 +22,8 @@ function TerisPresenter(canvasId, game) {
         const gameBoardCanvas = document.getElementById(_canvasId);
         const context = gameBoardCanvas.getContext("2d");
         context.beginPath();
-        context.fillStyle = "red";
-        context.arc(2 * _pixelPerCell + _cellRadius, 4 * _pixelPerCell + _cellRadius, _cellRadius, 0, Math.PI * 2);
+        context.fillStyle = _cell.color;
+        context.arc(_cell.x * _pixelPerCell + _cellRadius, _cell.y * _pixelPerCell + _cellRadius, _cellRadius, 0, Math.PI * 2);
         context.fill();
         context.closePath();
     };
