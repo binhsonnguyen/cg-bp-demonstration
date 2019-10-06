496ecaab1742de9310f1c05c2904dc2b9d5911d8
edward <son@binhsonnguyen.com>
2019-10-07 04:48:20 +0700

    Drop tetriminos from first row, and drop a new one each time it fall down on base.

--- classes/game.js
+++ classes/game.js
@@ -1,14 +1,18 @@
 function Game() {
     const _rows = 20;
     const _cols = 10;
-    let _tetriminos = new Tetriminos(3, 5);
+    let _tetriminos = new Tetriminos(3, 0);
 
     this.getRows = () => _rows;
     this.getCols = () => _cols;
     this.getTetriminos = () => _tetriminos;
 
     this.start = () => {
-        _tetriminos.moveDown();
+        if (_tetriminos.getY() === _rows) {
+            _tetriminos = new Tetriminos(3, 0);
+        } else {
+            _tetriminos.moveDown();
+        }
         setTimeout(this.start, 250);
     };
 }
