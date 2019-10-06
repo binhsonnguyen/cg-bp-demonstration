c21a9875e23865343bf8020e34478718ac4ea282
edward <son@binhsonnguyen.com>
2019-10-07 04:51:39 +0700

    Extract `isTetriminosOnBase` method.

--- classes/game.js
+++ classes/game.js
@@ -3,13 +3,14 @@ function Game() {
     const _cols = 10;
     const createNewTetriminos = () => new Tetriminos(3, 0);
     let _tetriminos = createNewTetriminos();
+    const isTetriminosOnBase = () => _tetriminos.getY() === _rows;
 
     this.getRows = () => _rows;
     this.getCols = () => _cols;
     this.getTetriminos = () => _tetriminos;
 
     this.start = () => {
-        if (_tetriminos.getY() === _rows) {
+        if (isTetriminosOnBase()) {
             _tetriminos = createNewTetriminos();
         } else {
             _tetriminos.moveDown();
